200c005b2692b68959e4d30dfae91a86f342c461
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2025-09-20 18:05:24 +0530

    Added search features in navbar

---
+++ Frontend/src/components/home/Search.jsx
@@ -0,0 +1,70 @@
+import React,{useState} from 'react';
+import {DatePicker,Space} from "antd";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../css/Home.css"
+
+const Search=()=> {
+
+    const {RangePicker}=DatePicker;
+    const [keyword,setKeyword]=useState({
+        city:"",
+        guest:"",
+        dateIn:"",
+        dateOut:""
+    });
+
+    const [value,setValue]=useState([]);
+
+    function searchHandler(e){
+        e.preventDefault();
+        setKeyword({city:"",guest:"",dateIn:"",dateOut:""})
+        setValue([])
+    }
+
+    function returnDates(date,dateString){
+        setValue([date[0],date[1]]);
+        updateKeyword("dateIn",dateString[0]);
+        updateKeyword("dateOut",dateString[1])
+    }
+
+    const updateKeyword = (field, value)=>{
+        setKeyword((prevKeyword)=>({
+            ...prevKeyword,
+            [field]:value
+        }));
+    }
+  return (
+    <div className='searchbar'>
+      <input
+      className='search'
+      id="search_destination"
+      placeholder='Search Destination'
+      type='text'
+      value={keyword.city}
+      onChange={(e)=> updateKeyword("city",e.target.value)}/>
+
+      <Space direction='vertical' size={12} className='search'>
+        <RangePicker 
+        value={value}
+        format="DD-MM-YYYY"
+        picker="date"
+        className="date_picker"
+        disabledDate={(current)=>{
+            return current && current.isBefore(Date.now(),"day")
+        }}
+        onChange={returnDates}
+        />
+      </Space>
+      <input className='search'
+      id='addguest'
+      placeholder='Add Guest'
+      type='number'
+      value={keyword.city}
+      onChange={(e)=>updateKeyword("guests",e.target.value)}
+      />
+      <span className='material-symbols-outlined searchicon' onClick={searchHandler}>search</span>
+    </div>
+  )
+}
+
+export default Search
